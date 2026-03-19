1acfec1149e46f2187074036dec0729ab5c7a7c7
Sakshi Raj <118617849+isakshiraj@users.noreply.github.com>
2026-03-19 14:47:16 +0530

    correctedAppjs

--- src/App.js
+++ src/App.js
@@ -1,2 +1,11 @@
-import { useState } from "react";
+import React from "react";
 
+function App() {
+  return (
+    <div>
+      <h1>Welcome to my website</h1>
+    </div>
+  );
+}
+
+export default App;
